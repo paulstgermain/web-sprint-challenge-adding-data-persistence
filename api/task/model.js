61be0263b1941fb5c6ac41d760f198856c4a3fbb
paulstgermain <paulg.stgermain@gmail.com>
2021-03-19 14:37:40 -0400

    completes tasks endpoint

--- api/task/model.js
+++ api/task/model.js
@@ -1,1 +1,26 @@
 // build your `Task` model here
+const db = require('../../data/dbConfig');
+
+const get = () => {
+    return db('tasks as t')
+        .join('projects as p', 'p.project_id', 't.project_id')
+        .select('t.task_id', 'task_description', 't.task_notes', 't.task_completed', 'p.project_name', 'p.project_description');
+};
+
+const getById = (id) => {
+    return db('tasks')
+        .where('task_id', id);
+};
+
+const add = (newTask) => {
+    return db('tasks')
+        .insert(newTask)
+        .then(id => {
+            return getById(id);
+        });
+};
+
+module.exports = {
+    get,
+    add
+};
--- api/task/router.js
+++ api/task/router.js
@@ -1,1 +1,26 @@
 // build your `/api/tasks` router here
+const router = require('express').Router();
+
+const Task = require('./model');
+
+router.get('/', (req, res) => {
+    Task.get()
+        .then(tasks => {
+            res.status(200).json(tasks);
+        })
+        .catch(err => {
+            res.status(500).json(err);
+        });
+});
+
+router.post('/', (req, res) => {
+    Task.add(req.body)
+        .then(newTask => {
+            res.status(201).json(newTask)
+        })
+        .catch(err => {
+            res.status(500).json(err);
+        });
+});
+
+module.exports = router;
