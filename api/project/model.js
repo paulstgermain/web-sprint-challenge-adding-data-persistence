14f516d57d6ff86dec6144bce23d7184c6ba6d5e
paulstgermain <paulg.stgermain@gmail.com>
2021-03-19 14:13:07 -0400

    completes projects endpoint

--- api/project/model.js
+++ api/project/model.js
@@ -1,1 +1,47 @@
 // build your `Project` model here
+const db = require('../../data/dbConfig');
+
+const get = async () => {
+    const projects = await db('projects');
+
+    const result = projects.map(project => {
+        if (project.project_completed === 0) {
+            return {
+                ...project,
+                project_completed: false
+            }
+        } else {
+            return {
+                ...project,
+                project_completed: true
+            }
+        }
+    })
+
+    return result;
+}
+
+const getById = async (id) => {
+    const project = await db('projects')
+        .where('project_id', id)
+        .first();
+
+    project.project_completed === 0 ? 
+    project.project_completed = false : 
+    project.project_completed = true;
+
+    return project;
+}
+
+const add = (project) => {
+    return db('projects')
+        .insert(project)
+        .then(id => {
+            return getById(id[0]);
+        })
+}
+
+module.exports = {
+    get,
+    add
+}
--- api/project/router.js
+++ api/project/router.js
@@ -1,1 +1,26 @@
 // build your `/api/projects` router here
+const router = require('express').Router();
+
+const Project = require('./model');
+
+router.get('/', (req, res) => {
+    Project.get()
+        .then(projects => {
+            res.status(200).json(projects);
+        })
+        .catch(err => {
+            res.status(500).json(err);
+        })
+})
+
+router.post('/', (req, res) => {
+    Project.add(req.body)
+        .then(newProject => {
+            res.status(200).json(newProject);
+        })
+        .catch(err => {
+            res.status(500).json(err);
+        })
+})
+
+module.exports = router;
